34c330af0b802f90cb81b2c92ed5ab264e801f4a
Jonathan Bowman <git@bowmanjd.com>
2020-04-30 06:52:33 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup  # type: ignore
 
 setup(
     name="enerator",
-    version="0.1",
+    version="0.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     entry_points={"console_scripts": ["enerator=enerator.commands:main"]},
